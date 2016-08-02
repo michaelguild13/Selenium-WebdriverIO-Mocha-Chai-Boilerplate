4bb14949a7a94daa80c5047f00d9e34a8568f64b
Michael Guild <michaelguild13@gmail.com>
2016-08-02 16:27:24 -0400

    removed the jar in favor of the node package

--- test.js
+++ test.js
@@ -4,12 +4,13 @@ var options = {
         browserName: 'firefox'
     }
 };
-
+console.log(webdriverio.init());
 webdriverio
     .remote(options)
     .init()
     .url('http://www.google.com')
     .getTitle().then(function(title) {
+        console.log('asdjflaksjdflksadjf');
         console.log('Title was: ' + title);
     })
     .end();
