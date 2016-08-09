3e9ecded61c9b16fabacdd642894248f9b7778cc
Michael Guild <michaelguild13@gmail.com>
2016-08-09 11:34:20 -0500

    just doing a small commit

--- href.js
+++ href.js
@@ -10,15 +10,6 @@ var webdriverio = require('webdriverio');
 var client = webdriverio.remote(settings).init()
   .url('http://localhost:8000/')
   // .url('https://www.arcpublishing.com/')
-  //
-//   .elements('a', function(err,res) {
-//     var i = 0;
-//     res.value.forEach(function(elem) {
-//         client.elementIdAttribute(elem, 'href', function(err,res) {
-//             console.log(++i, res.value);
-//         });
-//     });
-// });
   .elements('a')
   .then(function(links) {
     for (var i=0; i<links.value.length; i++) {
@@ -29,7 +20,7 @@ var client = webdriverio.remote(settings).init()
           console.log(result.value);
           fetch(result.value)
             .then( function(res){
-              console.log(res.status);
+              console.log(res);
             })
             .catch( function(err){
               console.log(err || 'error');
@@ -37,15 +28,3 @@ var client = webdriverio.remote(settings).init()
         });
     }
   })
-
-  // function getAllHrefs(driver, cssValue){
-  //     var selector;
-  //     if(!cssValue)   selector = By.tagName('a');
-  //     else            selector = By.css(cssValue);
-  //     return driver.findElements(selector).then(function(oLinks){
-  //         var allPromises = oLinks.map(function(oLink){
-  //             return oLink.getAttribute('href');
-  //         });
-  //         return Driver.promise.all(allPromises);
-  //     });
-  // }
