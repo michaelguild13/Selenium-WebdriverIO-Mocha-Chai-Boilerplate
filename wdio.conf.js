11901838d84425d0ca715f264dd5c95a89441f8f
Michael Guild <michaelguild13@gmail.com>
2017-02-08 11:30:46 -0700

    added some comments for users

--- wdio.conf.js
+++ wdio.conf.js
@@ -44,7 +44,7 @@ exports.config = {
         // 5 instance gets started at a time.
         maxInstances: 5,
         //
-        browserName: 'phantomjs'
+        browserName: 'phantomjs' // options: chrome || firefox || phantomjs
     }],
     //
     // ===================
